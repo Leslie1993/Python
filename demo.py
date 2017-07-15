68150755046bb596cbf927169628af2e53a40667
Leslie <929091680@qq.com>
2017-07-15 23:47:25 +0800

    123

---
+++ demo.py
@@ -0,0 +1,13 @@
+source =[]
+for x in range(5):
+	source.append(x)
+source=source[:-1:2]
+print(source)
+list=[x**2 for x in range(1,10)]
+print(list)
+dic={1:1,2:2}
+print(dic)
+for x,y in dic.items():
+	print(x)
+	print(y)
+gen=[]
---
+++ module1.py
@@ -0,0 +1,11 @@
+#12343
+import sys
+
+def func():
+    print(100)
+
+
+for i in range(1, 100):
+    func()
+for item in sys.path:
+    print(item)

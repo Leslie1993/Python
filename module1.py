abe258b8dc07fc367f38709366040e08338866d1
Leslie1993 <929091680@qq.com>
2017-07-31 22:53:19 +0800

    添加测试代码

--- module1.py
+++ module1.py
@@ -10,11 +10,14 @@ def func(indx):
     '''Prints the maximum of two numbers.
 
     The two values must be integers.'''
-
+    if(__name__=="__main__"):
+        print("1")
     print(indx)
 
+if(__name__=="__main__"):
+    print("1")
 
-for i in range(1, 100):
-    func(i)
-for item in sys.path:
-    print(item)
+# for i in range(1, 100):
+#     func(i)
+# for item in sys.path:
+#     print(item)
---
+++ test.py
@@ -0,0 +1,11 @@
+from module1 import func
+func(100)
+
+l=[i for i in range(10)]
+print(l)
+
+print(l[:])
+print(l[:-1])
+print(l[1:2])    
+print(l[::3])
+print(l[::-1])
